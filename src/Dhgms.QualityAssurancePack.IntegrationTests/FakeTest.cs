9a21aea9b01171faa67ae0674408f45cb0aa4942
David Vreony <dpvreony@users.noreply.github.com>
2026-01-03 22:16:23 +0000

    housekeeping: switch to common workflow (#304)
    * switch to common workflow
    
    Updated CI workflow for .NET, adjusting permissions and removing unnecessary steps.
    
    * add build control files
    
    * Update Dhgms.QualityAssurancePack.csproj
    
    * enable stylecop
    
    * Create stylecop.json
    
    * add testing stubs

---
+++ src/Dhgms.QualityAssurancePack.IntegrationTests/FakeTest.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) 2015 - 2026 DPVreony and Contributors. All rights reserved.
+// DPVreony and Contributors licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for full license information.
+
+using Xunit;
+
+namespace Dhgms.QualityAssurancePack.IntegrationTests
+{
+    /// <summary>
+    /// Fake test, to satisfy CI.
+    /// </summary>
+    public sealed class FakeTest
+    {
+        /// <summary>
+        /// Fake test, to satisfy CI.
+        /// </summary>
+        [Fact]
+        public void DoFakeTest()
+        {
+            // fake test, to satisfy CI.
+            // other PRs will add real tests.
+            Assert.True(true);
+        }
+    }
+}
